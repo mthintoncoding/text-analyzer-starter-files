04790404272dd14a3cde28b1bbb82bd3f9b7d296
mthintoncoding <mthinton@gmail.com>
2018-04-02 18:44:18 -0400

    Re-did the text-analyzer exercise. User enters text and web app displays to user how many unique words there are, how many total words there are and average word length

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// your code here!
+function getAverageWordLength(userProse){
+  var totalLength = userProse.join("").length;
+  return (totalLength / userProse.length).toFixed(2);
+}
+
+
+function uniqueWordCount(userProse){
+  var results = {};
+  var uniqueWords = [];
+  userProse.forEach(function(word){
+    results[word] = word;
+  });
+    for(var j = 0; j < userProse.length; j++){
+      if(userProse[j] === results[userProse[j]]){
+        results[userProse[j]] = 0;
+      }
+      results[userProse[j]]++
+    }
+
+    for(var word in results){
+      if(results[word] == 1){
+        uniqueWords.push(word);
+      }
+    }
+    return uniqueWords.length;
+}
+
+function textAnalyzer(userProse){
+  $('.word-count').text(userProse.length);
+  $('.text-report').removeClass('hidden');
+  $('.unique-word-count').text(uniqueWordCount(userProse));
+  $('.word-length').text(getAverageWordLength(userProse));
+}
+
+function createTextArray(text){
+  var userArray = text.toLowerCase().split(" ");
+  textAnalyzer(userArray);
+}
+
+function getUserText(){
+  $('.user-text-form').submit(function(event){
+    event.preventDefault();
+    var userText = $("#user-text").val();
+    createTextArray(userText);
+  })
+}
+
+$(getUserText)
